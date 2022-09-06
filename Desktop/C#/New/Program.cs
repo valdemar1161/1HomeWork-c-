a21a688ace19fe978865d7b6673e4fb595106f61
Valentin <dirk_31@mail.ru>
2022-09-06 21:02:23 +0700

    Home work1

---
+++ Desktop/C#/New/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num * num;
+System.Console.WriteLine($"Квадрат числа {num} -> {result}");
+
+
